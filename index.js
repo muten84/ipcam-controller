5ed0db92f482a13671fe3a86f3eae10c49516546
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-08 22:16:57 +0200

    actions
    actions

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var IPCamera = require("./src/SricamService.js");
-var Promise = require("promise");
+var camera=require('./src/actions.js')
 
 
 //initialize camera
@@ -11,24 +10,5 @@ var config = {
   pwd: "manga123"
 }
 
-var service = new IPCamera(config);
-
-function done(){
-  console.log("done");
-}
-
-function moveLeft(){
-  var promise = new Promise(function (resolve, reject) {
-  IPCamera.execute(service,"moveLeft",3000).then(resolve);
-  });
-  return promise;
-}
-
-function stopLeft(){
-  var promise = new Promise(function (resolve, reject) {
-  IPCamera.execute(service,"stopLeft",3000).then(resolve);
-  });
-  return promise;
-}
-
-moveLeft().then(stopLeft).then(done);
+camera.init(config);
+camera.moveLeft().then(camera.stopLeft).then(camera.done);
---
+++ src/actions.js
@@ -0,0 +1,30 @@
+var IPCamera = require("./SricamService.js");
+var Promise = require("promise");
+
+var service = null;
+
+function init(config){
+   service = new IPCamera(config);
+}
+
+function moveLeft(){
+  var promise = new Promise(function (resolve, reject) {
+  IPCamera.execute(service,"moveLeft",3000).then(resolve);
+  });
+  return promise;
+}
+
+function stopLeft(){
+  var promise = new Promise(function (resolve, reject) {
+  IPCamera.execute(service,"stopLeft",3000).then(resolve);
+  });
+  return promise;
+}
+
+function done(){
+  console.log("done");
+}
+
+var exp = {init,moveLeft,stopLeft,done}
+
+module.exports=exp;
