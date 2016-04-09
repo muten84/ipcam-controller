51887c24a1f6417e9453b9a890b28b5df0abaf10
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-09 17:07:10 +0200

    scheduler

--- ipcam.js
+++ ipcam.js
@@ -16,14 +16,17 @@ program
 .option('-a, --address <address>', 'The camera ip address')
 .option('-m, --modelType <modelType>', 'The camera model type (e.g.: sricamAF004)')
 .option('-d, --duration <duration>', 'The move duration in milliseconds')
+.option('-s, --schedule <schedule> <count>', 'Schedule given action for n times during schedule times: for instance \'-s 30 3\' schedule for 3 times given action from now to now+30 minutes')
 .action(function(action){
   var user = program.user;
   var pwd = program.password;
   var cameraType = program.modelType;
   var ip = program.address;
   var duration = program.duration;
-   console.log('camera: %s action: %s user: %s pwd: %s ip: %s, duration: %s',
-        cameraType, action, user, pwd, ip, duration);
+  var schedule = program.schedule;
+  var count = program.count;
+   console.log('camera: %s action: %s user: %s pwd: %s ip: %s, duration: %s, schedule: %s, count: %s',
+        cameraType, action, user, pwd, ip, duration, schedule, count);
   var config = {
   ip: ip,
   name: "Camera",
@@ -38,11 +41,10 @@ program
     camera.moveLeft().then(camera.stopLeft);
     break;
    case 'right':
-    camera.moveRight().then(camera.stopRight); 
+    camera.moveRight().then(camera.stopRight);
     break;
    default:
-    break; 
-  }  
+    break;
+  }
 })
 .parse(process.argv);
- 
