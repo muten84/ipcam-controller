842afd8bd2668b6f1a0f13742147ff435d3a50a6
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-09 17:39:19 +0200

    merge

--- ipcam.js
+++ ipcam.js
@@ -17,7 +17,7 @@ program
 .option('-m, --modelType <modelType>', 'The camera model type (e.g.: sricamAF004)')
 .option('-d, --duration <duration>', 'The move duration in milliseconds')
 .option('-s, --schedule <schedule>', 'Schedule given action for n times during schedule times: for instance \'-s 30\' schedule for 1 time the given action from now to now+30 minutes')
-.option('-c' ,'--count <count>', 'Use it in combination with schedule to configure how many times during schedule time the given action should start: for instance -s 30 -c 3 schedule fo 3 times the given action during 30 minutes from now where now is the command start execution time')
+.option('-c, --count <count>', 'Use it in combination with schedule to configure how many times during schedule time the given action should start: for instance -s 30 -c 3 schedule fo 3 times the given action during 30 minutes from now where now is the command start execution time')
 .action(function(action){
   var user = program.user;
   var pwd = program.password;
