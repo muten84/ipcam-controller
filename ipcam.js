5bd8f3ec57e437f4b9bfd92f6788d2502e107246
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-10 16:21:47 +0200

    api implementation

--- ipcam.js
+++ ipcam.js
@@ -12,6 +12,7 @@ program
 .option('-d, --duration <duration>', 'The move duration in milliseconds')
 .option('-s, --schedule <schedule>', 'Schedule given action for n times during schedule times: for instance \'-s 30\' schedule for 1 time the given action from now to now+30 minutes')
 .option('-c, --count <count>', 'Use it in combination with schedule to configure how many times during schedule time the given action should start: for instance -s 30 -c 3 schedule fo 3 times the given action during 30 minutes from now where now is the command start execution time')
+.option('-n --cameraName <cameraName>','The camera name identifier')
 .action(function(action){
   var user = program.user;
   var pwd = program.password;
@@ -20,18 +21,19 @@ program
   var duration = program.duration;
   var schedule = program.schedule;
   var count = program.count;
+  var name = program.name;
   console.log('camera: %s action: %s user: %s pwd: %s ip: %s, duration: %s, schedule: %s, count: %s',
   cameraType, action, user, pwd, ip, duration, schedule, count);
   var config = {
     ip: ip,
-    name: "Camera",
+    name: name||"Camera",
     type: cameraType,
     user: user,
     pwd: pwd,
     duration: duration
   }
   var camera = new IPCamera();
-  camera.createCamera(name,type,config);
+  camera.createCamera(config.name,type,config);
   if(!schedule){
     doNow(camera,action,duration);
   }else{
