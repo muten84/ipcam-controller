76b25b57b7e478e63ef8abbc38ca6d131afcc02d
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-10 16:36:42 +0200

    api test

--- test/apiTest.js
+++ test/apiTest.js
@@ -1,13 +1,16 @@
-var camera = require('../index.js');
+var IPCamera = require('../index.js');
 
 var config = {
-  ip: '127.0.0.1',
+  ip: 'cameraIp',
   name: "Camera",
   type: 'sricamAP004',
   user: 'admin',
-  pwd: 'password',
+  pwd: 'troppsecret',
   duration: 3000
 }
 
+var camera = new IPCamera();
 camera.createCamera("test", "sricamAP004",config);
-camera.moveLeftFor(500).then(function(){camera.moveRightFor(500)});
+camera.moveLeftFor(500).then(function(){
+	camera.moveRightFor(500);
+});
