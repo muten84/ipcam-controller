caae6e699487d8311a9c11d6cc817cf417e1b895
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-10 12:05:19 +0200

    correct camera model type

--- src/CameraFactory.js
+++ src/CameraFactory.js
@@ -2,6 +2,15 @@ var cameraInstance = null;
 var CameraControl = require("./CameraControl.js");
 
 function createSingletonCamera(name, type, props){
+ var spec =  null;
+ try{
+  spec = require(""+type);
+ }
+ catch(err){
+  spec =null;
+ }
+ if(!spec)
+  var module = require(type);
   var spec = require("./"+type+".js");
   if(cameraInstance===null){
     cameraInstance = new CameraControl(name);
