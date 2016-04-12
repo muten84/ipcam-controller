eeb6c3951b1326d5a9e279fc1ecc5c4734280f4f
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-12 16:18:39 +0200

    fix after refactoring

--- src/IpCameraController.js
+++ src/IpCameraController.js
@@ -3,10 +3,10 @@
 * @module IpCameraController
 * @author Luigi Bifulco
 */
-var factory = require('./src/CameraFactory.js')
-var Service = require('./src/CameraService.js')
-var Scheduler = require('./src/scheduler.js')
-var Types = require('./src/scheduler.js')
+var factory = require('.CameraFactory.js')
+var Service = require('.CameraService.js')
+var Scheduler = require('.scheduler.js')
+var Types = require('.scheduler.js')
 var Promise = require("promise");
 
 /**
